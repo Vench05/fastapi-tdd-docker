d34ad867a1ce9a3f155d9e77cb9c31413fc2c14b
Ralf <ralfgvenchbillones@gmail.com>
2021-10-24 22:30:13 +0800

    Initial commit with CI

---
+++ project/app/config.py
@@ -0,0 +1,19 @@
+import logging
+from functools import lru_cache
+
+from pydantic import AnyUrl, BaseSettings
+
+
+log = logging.getLogger('uvicorn')
+
+
+class Settings(BaseSettings):
+    environment: str = 'dev'
+    testing: bool = False
+    database_url: AnyUrl
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info('Loading config setting the environment...')
+    return Settings()
---
+++ project/test/test_ping.py
@@ -0,0 +1,18 @@
+from urllib import response
+from app import main
+from app.main import FastAPI
+
+def test_ping(test_app: FastAPI):
+    # Given
+    # test_app
+
+    # When
+    response = test_app.get('/ping')
+
+    # Then
+    assert response.status_code == 200
+    assert response.json() == {
+                                'ping': 'pong!',
+                                'environment': 'dev',
+                                'testing': True
+                                }
