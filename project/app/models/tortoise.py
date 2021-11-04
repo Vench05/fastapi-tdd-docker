1cdcd0057bb24e47ad50f1f53c8c6aaa69943ed5
Ralf <ralfgvenchbillones@gmail.com>
2021-11-04 15:51:01 +0800

    update create to created

--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -5,7 +5,7 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 class TextSummary(models.Model):
     url = fields.TextField()
     summary = fields.TextField()
-    create_at = fields.DatetimeField(auto_now_add=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
 
     def __str__(self):
         return self.url
--- project/test/test_summaries.py
+++ project/test/test_summaries.py
@@ -40,7 +40,7 @@ def test_read_summary(test_app_with_db: FastAPI):
     assert response_dict["id"] == summary_id
     assert response_dict["url"] == "https://foo.bar"
     assert response_dict["summary"]
-    assert response_dict["create_at"]
+    assert response_dict["created_at"]
 
 
 def test_read_summary_incorrect_id(test_app_with_db: FastAPI):
@@ -121,7 +121,7 @@ def test_update_summary(test_app_with_db: FastAPI):
     assert response_dict["id"] == summary_id
     assert response_dict["url"] == "https://foo.bar"
     assert response_dict["summary"] == "updated!"
-    assert response_dict["create_at"]
+    assert response_dict["created_at"]
 
 
 @pytest.mark.parametrize(
